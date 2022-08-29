b23dcdc3bf4f2d0cd98325c7e5868bc57279804a
lucas <dantaslucas337@gmail.com>
2022-08-29 18:11:17 -0300

    Mudanças na barra de navegação

---
+++ src/componentes/footer/footer.tsx
@@ -0,0 +1,42 @@
+import { Grid, Typography } from "@material-ui/core";
+import { Box } from "@mui/material";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import GitHubIcon from '@mui/icons-material/GitHub';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+
+function Footer() {
+    return (
+        <>
+            <Grid container direction="row" justifyContent="center" alignItems="center">
+                <Grid alignItems="center" item xs={12}>
+                    <Box style={{ backgroundColor: "#3F51B5", height: "120px", background: '#74849C', color: '#F2F2F2' }} className="footer-container">
+                        <Box paddingTop={1} display="flex" alignItems="center" justifyContent="center">
+                            <Typography variant="h5" align="center" gutterBottom style={{ color: "white" }}>Me siga nas redes sociais:</Typography>
+                        </Box>
+                        <Box display="flex" alignItems="center" justifyContent="center">
+                            <a href="https://github.com/DantasZo" target="_blank">
+                                <GitHubIcon style={{ fontSize: 60, color: "white" }} />
+                            </a>
+                        </a>
+                        <a href="https://www.linkedin.com/in/lucas-dantas-6837b9227/" target="_blank">
+                            <LinkedInIcon style={{ fontSize: 60, color: "white" }} />
+                        </a>
+                    </Box>
+                </Box>
+                <Box style={{ backgroundColor: "#212f49", height: "60px" }}>
+                    <Box paddingTop={1}>
+                        <Typography variant="subtitle2" align="center" gutterBottom style={{ color: "white" }} >© 2020 Copyright:</Typography>
+                    </Box>
+                    <Box>
+                        <a target="_blank" href="https://brasil.generation.org">
+                            <Typography variant="subtitle2" gutterBottom style={{ color: "white" }} align="center">brasil.generation.org</Typography>
+                        </a>
+                    </Box>
+                </Box>
+            </Grid>
+        </Grid>
+    </>
+  );
+}
+
+export default Footer;
--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,12 +1,31 @@
-import React from "react";
-import './Home.css';
+import "./Home.css";
+import { Grid, Button, Typography } from "@material-ui/core";
+import { Box } from "@mui/material";
+
 
 function Home() {
-    return (
-        <>
-            <h1 className='titulo' >  Home  </h1>
-        </>
-    )
+  return (
+    <>
+      <Grid container direction="row" justifyContent="center" alignItems="center" className="container" style={{ backgroundColor: "#3F51B5" }}>
+                <Grid alignItems="center" item xs={6}>
+                    <Box paddingX={20} >
+                        <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" style={{ color: "white", fontWeight: "bold" }}>Seja bem-vinde!</Typography>
+                        <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" style={{ color: "white", fontWeight: "bold" }}>Blog pessoal com meus estudos sobre linguagens de programação e tecnologias.</Typography>
+                    </Box>
+                    <Box display="flex" justifyContent="center">
+                        <Box marginRight={1}>
+                        </Box>
+                        <Button variant="outlined" className="btn" style={{ borderColor: "white", backgroundColor: "#3F51B5", color: "white" }}>Ver Postagens</Button>
+                    </Box>
+                </Grid>
+                <Grid item xs={6} >
+                    <img src="https://pbs.twimg.com/media/FbBPK--WAAARYvn?format=png&name=small" alt="" width="500px" height="512px" />
+                </Grid>
+                <Grid xs={12} style={{ backgroundColor: "white" }}>
+                </Grid>
+            </Grid>
+    </>
+  );
 }
 
 export default Home;
