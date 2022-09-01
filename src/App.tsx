7aa96660dfbabd218d913984bc1a74470c413297
lucas <dantaslucas337@gmail.com>
2022-09-01 10:04:42 -0300

    Roates adicionadas

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import React from 'react';
 import './App.css';
 import Home from './paginas/home/Home';
 import Footer from './componentes/footer/Footer'
 import Navbar from "./componentes/navbar/Navbar";
-
+import Login from "./paginas/login/Login";
 
 function App() {
   return (
-  <>
-
-  <Navbar />
-  <Home />
-  <Footer />
-
-  </>
+    <Router>
+      <Navbar />
+      <div style={{ minHeight: "100vh" }}>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </div>
+      <Footer />
+    </Router>
   );
 }
 
